66285af1b926fe07f9568cd6e1bf9761e843b215
kdv1995 <tribeofdanel@gmail.com>
2024-06-21 18:48:21 +0300

    now must work

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,14 +1,13 @@
 import { FaHome } from 'react-icons/fa';
 import { CiCircleList } from 'react-icons/ci';
-import { To } from 'react-router-dom';
 
 interface INavigationItem {
   label: string;
   icon: JSX.Element;
-  link: To | string;
+  link: string;
 }
 
-export const APP_ROUTES: { [key: string ]: INavigationItem } = {
+export const APP_ROUTES: { [key: string]: INavigationItem } = {
   main: {
     link: '/main',
     label: 'Main',
